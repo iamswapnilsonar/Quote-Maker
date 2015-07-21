4a37e0acda43798385af96f9f3ffa69119cc4545
Swapnil Sonar <swap.m.sonar@gmail.com>
2015-07-21 20:10:38 +0530

    Create PackageInfoBean.java

---
+++ PackageInfoBean.java
@@ -0,0 +1,21 @@
+import android.content.pm.ApplicationInfo;
+import android.graphics.drawable.Drawable;
+
+/**
+ * @author Swapnil Sonar
+ * @Date 01/07/2015
+ */
+public class PackageInfoBean {
+    
+	public CharSequence mAppName = "";
+    public String mAppPackageName = "";
+    public String mVersionName = "";
+    public int mVersionCode = 0;
+    public Drawable icon;
+    public String mDataDir = "";
+    public long cacheSize = 0;
+    public ApplicationInfo mApplicationInfo;
+    public String mApkFilePath = "";
+    public long mApkFileSize = 0;
+    
+}
